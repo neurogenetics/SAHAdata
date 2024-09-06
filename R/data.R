46008ec5125aa1aa4432e38c8dd0f1f08b0c3031
Dom Acri <46611562+domjacri@users.noreply.github.com>
2024-09-06 15:28:00 -0400

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,438 @@
+#' Data containing Markers from ABC Cerebellum
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(CB_Markers) #will be lazy loading
+"CB_Markers"
+
+#' Data containing Markers from ABC CTXsp
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(CTXsp_Markers) #will be lazy loading
+"CTXsp_Markers"
+
+#' Data containing Markers from ABC Hippocampus
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(HPF_Markers) #will be lazy loading
+"HPF_Markers"
+
+#' Data containing Markers from ABC Hypothalamus
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(HY_Markers) #will be lazy loading
+"HY_Markers"
+
+#' Data containing Markers from ABC Isocortex
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(ISOCTX_Markers) #will be lazy loading
+"ISOCTX_Markers"
+
+#' Data containing Markers from ABC Midbrain
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(MB_Markers) #will be lazy loading
+"MB_Markers"
+
+#' Data containing Markers from ABC MY
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(MY_Markers) #will be lazy loading
+"MY_Markers"
+
+#' Data containing Markers from ABC Olfactory Bulb
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(OLF_Markers) #will be lazy loading
+"OLF_Markers"
+
+#' Data containing Markers from ABC P
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(P_Markers) #will be lazy loading
+"P_Markers"
+
+#' Data containing Markers from ABC Palladium
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(PAL_Markers) #will be lazy loading
+"PAL_Markers"
+
+#' Data containing Markers from ABC Striatum
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(STR_Markers) #will be lazy loading
+"STR_Markers"
+
+#' Data containing Markers from ABC Thalamus
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene*cluster ID}
+#'  \item{p_val}{Uncorrected p value for gene in cluster-of-interest vs. ALL}
+#'  \item{avg_log2FC}{Average log2FC of expression for gene in cluster-of-interest vs. ALL}
+#'  \item{pct.1}{Percent of cells in cluster-of-interest to express gene}
+#'  \item{pct.2}{Percent of cells not in cluster-of-interest to express gene}
+#'  \item{p_val_adj}{Adjusted p value for gene in cluster-of-interest vs. ALL}
+#'  \item{cluster}{Cluster-of-interest}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(TH_Markers) #will be lazy loading
+"TH_Markers"
+
+#' Data containing AvgExp from ABC Cerebellum
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(CB_AvgExp) #will be lazy loading
+"CB_AvgExp"
+
+#' Data containing AvgExp from ABC CTXsp
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(CTXsp_AvgExp) #will be lazy loading
+"CTXsp_AvgExp"
+
+#' Data containing AvgExp from ABC Hippocampus
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(HPF_AvgExp) #will be lazy loading
+"HPF_AvgExp"
+
+#' Data containing AvgExp from ABC Hypothalamus
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(HY_AvgExp) #will be lazy loading
+"HY_AvgExp"
+
+#' Data containing AvgExp from ABC Isocortex
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(ISOCTX_AvgExp) #will be lazy loading
+"ISOCTX_AvgExp"
+
+#' Data containing AvgExp from ABC Midbrain
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(MB_AvgExp) #will be lazy loading
+"MB_AvgExp"
+
+#' Data containing AvgExp from ABC MY
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(MY_AvgExp) #will be lazy loading
+"MY_AvgExp"
+
+#' Data containing AvgExp from ABC Olfactory bulb
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(OLF_AvgExp) #will be lazy loading
+"OLF_AvgExp"
+
+#' Data containing AvgExp from ABC P
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}#'
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(P_AvgExp) #will be lazy loading
+"P_AvgExp"
+
+#' Data containing AvgExp from ABC Palladium
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}#'
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(PAL_AvgExp) #will be lazy loading
+"PAL_AvgExp"
+
+#' Data containing AvgExp from ABC Striatum
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}#'
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(STR_AvgExp) #will be lazy loading
+"STR_AvgExp"
+
+#' Data containing AvgExp from ABC Thalamus
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of gene}
+#'  \item{ASSAY.gCLUSTER_NAME}{# of columns corresponding to each unique CLUSTER_NAME}
+#'  \item{gene}{ENSMBL ID for Gene}
+#'  \item{SYMBOL}{Gene Symbol}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(TH_AvgExp) #will be lazy loading
+"TH_AvgExp"
+
+#' Data containing Allen Brain Cell Atlas Metadata required for SAHA
+#'
+#' Data contains
+#' @format A dataframe with ...:
+#'  \describe{
+#'  \item{X}{Running index of celltype}
+#'  \item{class}{Highest level of annotation}
+#'  \item{neurotransmitter}{Second highest level of annotation}
+#'  \item{library_method}{Chemistry used to sequence cells.}
+#'  \item{class_color}{ABC determined color of the class}
+#'  \item{neurotransmitter_color}{ABC determiend color of nt}
+#'  \item{sublcass_per}{period deliminted subclass name}
+#'  \item{sublcass_spa}{space deliminted subclass name}}
+#' @source {https://portal.brain-map.org/atlases-and-data/rnaseq}
+#'
+#' @examples
+#' data(ABC_meta) #will be lazy loading
+"ABC_meta"
